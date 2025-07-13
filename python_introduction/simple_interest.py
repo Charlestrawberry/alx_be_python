55f15534b6a5734b1eb6c206ed547a013fd95e91
Oduks <charlestrawb_odus@yahoo.com>
2025-07-13 12:28:05 +0100

    fixed var errors

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,8 +1,8 @@
 ### calculating simple interest
-pricipal = 1000  # Principal amount
-rate = 0.05  # Rate of interest (5%)
-time = 3  # Time in years
+P = 1000  # Principal amount
+R = 0.05  # Rate of interest (5%)
+T = 3  # Time in years
 
-interest = (pricipal * rate * time)  # Simple Interest formula
+I = (P * R * T)  # Simple Interest formula
 
-print(f"The simple interest is: {interest}") 
+print(f"The simple interest is: {I}") 
