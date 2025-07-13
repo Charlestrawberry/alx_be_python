11744a707e48e98b97dc8f9a0d4c3330d9d49e8a
Oduks <charlestrawb_odus@yahoo.com>
2025-07-13 12:21:06 +0100

    added simple interest file

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,8 @@
+### calculating simple interest
+pricipal = 1000  # Principal amount
+rate = 0.05  # Rate of interest (5%)
+time = 3  # Time in years
+
+interest = (pricipal * rate * time)  # Simple Interest formula
+
+print(f"The simple interest is: {interest}") 
