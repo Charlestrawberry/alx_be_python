84da12c1fee3433943462d97f1a88a5161aca472
Oduks <charlestrawb_odus@yahoo.com>
2025-08-03 23:16:05 +0100

    added calc function/division

---
+++ programming_paradigm/robust_division_calculator.py
@@ -0,0 +1,14 @@
+def safe_divide(numerator, denominator):
+    try: 
+        num = float(numerator)
+        denom = float(denominator)
+
+    except ValueError:
+        return "Error: Input must be a number"
+    
+    try:
+        result = num /denom
+        return f"result: {result}"
+    except ZeroDivisionError:
+        return "Error: Cannot divide by zero"
+    
