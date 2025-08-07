a1cdd5c73487a46745547e2e593058d26218f8cb
Oduks <charlestrawb_odus@yahoo.com>
2025-08-07 21:46:09 +0100

    more output bugs fixed

--- oop/main.py
+++ oop/main.py
@@ -2,7 +2,7 @@ from book_class import Book
 
 def main():
     # create book instance
-    book = Book("1984", "Charles Oduks", 2025)
+    book = Book("1984", "George Orwell", 1949)
     
     # Usage of __str__ mthd
     print(book)
