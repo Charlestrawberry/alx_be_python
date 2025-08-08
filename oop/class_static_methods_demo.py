9718569cdcbde48c5f480e67b0cfd8989b673f67
Oduks <charlestrawb_odus@yahoo.com>
2025-08-08 20:24:51 +0100

    added lesson on @static and @class decorators

---
+++ oop/class_static_methods_demo.py
@@ -0,0 +1,22 @@
+class Calculator:
+    calculation_type = "Arithmetic Operations" #class attribute
+        
+    @staticmethod 
+    def add(a, b):
+        sum = a + b
+        return sum
+    
+    @classmethod
+    def multiply(cls, a, b):
+        product = a * b
+        print(f"Calculation type: {cls.calculation_type}")
+        return product
+
+if __name__ == "__main__":
+    # using the @static mthd
+    sum_result = Calculator.add(10, 5)
+    print(f"The sum is: {sum_result}")
+
+    # using the @class mthd
+    product_result = Calculator.multiply(10, 5)
+    print(f"The product is: {product_result}")
