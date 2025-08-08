1b0c9637a5f258baac20991c39cf30a5d5cbb392
Oduks <charlestrawb_odus@yahoo.com>
2025-08-08 19:19:07 +0100

    fixed bug issue on indent

--- oop/polymorphism_demo.py
+++ oop/polymorphism_demo.py
@@ -29,5 +29,5 @@ if __name__ == "__main__":
         Rectangle(10, 5),
         Circle(7)
     ]
-for shape in shapes:
-    print(f"The area of the {shape.__class__.__name__} is: {shape.area()}")
+    for shape in shapes:
+        print(f"The area of the {shape.__class__.__name__} is: {shape.area()}")
